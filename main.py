f904bdb8717368029ce01c575346609264e7679a
Ngô Bảo Long <nblong23@.clc.fitus.edu.vn>
2025-02-20 19:54:14 +0700

    fixing heuristic A*

--- main.py
+++ main.py
@@ -5,18 +5,21 @@ import os
 from BFS import *
 from gbfs import *
 from helper import *
+from A_star import *
 
 def main():
-    file_name = "./Inputs/input-03.txt"
+    file_name = "./Inputs/input-01.txt"
     grid, costs = GetMapFromFile(file_name)
     print(grid)
     print(costs)
     ares_pos, stones, switches = find_pos(grid)
     print(stones)
+    print(switches)
 
     if ares_pos and stones:
         print("1. Breadth First Search")
         print("2. Greedy Best First Search")
+        print("3. A*")
         choice = int(input("Choose algorithm you want to use: "))
         
         path, totalCost = None, None
@@ -25,6 +28,8 @@ def main():
             path, totalCost = order_bfs(grid, ares_pos, stones, costs)
         elif choice == 2:
             path, totalCost = launch(file_name)
+        elif choice == 3:
+            path, totalCost = order_A_star(grid, ares_pos, stones, costs, switches)
             
             
         if path:
@@ -41,3 +46,4 @@ memory_used = process.memory_info().rss / 1024**2
 
 print(f"Time exceeded: {round(elapsed_time, 3)}")
 print(f"Memory: {memory_used:.2f} MB")
+
