82e26f4cb528f07960332a702dfcfc3e119cbe46
Tran Quoc Thien <tqthien23@clc.fitus.edu.vn>
2025-02-20 18:06:26 +0700

    fix time exceeded

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import timeit
+import time
 import psutil
 import os
 
@@ -6,8 +6,9 @@ from BFS import *
 from gbfs import *
 from helper import *
 
+
 def main():
-    file_name = "./Inputs/input-03.txt"
+    file_name = "./Inputs/input-02.txt"
     grid, costs = GetMapFromFile(file_name)
     print(grid)
     print(costs)
@@ -21,23 +22,33 @@ def main():
         
         path, totalCost = None, None
         
+        start_time = time.time()
+        
         if choice == 1:
             path, totalCost = order_bfs(grid, ares_pos, stones, costs)
+            
         elif choice == 2:
             path, totalCost = launch(file_name)
             
-            
+        elapsed_time = time.time() - start_time
+        
         if path:
             print("\nPath found:", ''.join(path))
             print("Total cost: ", totalCost)
         else:
             print("\nNo valid path found.")
+            
+        process = psutil.Process(os.getpid())  
+        memory_used = process.memory_info().rss / 1024**2 
+
+        print(f"Time exceeded: {round(elapsed_time, 3)}")
+        print(f"Memory: {memory_used:.2f} MB")
+            
+
     else:
         print("\nInvalid map: Missing Ares or stones.")
+        
 
-elapsed_time = timeit.timeit(main, number=1)
-process = psutil.Process(os.getpid())  
-memory_used = process.memory_info().rss / 1024**2 
 
-print(f"Time exceeded: {round(elapsed_time, 3)}")
-print(f"Memory: {memory_used:.2f} MB")
+if __name__ == "__main__":
+    main()
